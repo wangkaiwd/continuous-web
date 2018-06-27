ceaea10d8124bc98dc910f66960fd17e52651e40
wangkaiwd <1162486176@qq.com>
2018-06-28 00:47:05 +0800

    parcel运行出现问题

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Button from '../button';
+
+Vue.component('g-button', Button)
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      message: 'hi',
+    }
+  }
+})
