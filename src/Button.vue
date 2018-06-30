0e10fb3d528d5408d7af9b8da2fc0754d77980bb
wangkaiwd <1162486176@qq.com>
2018-06-30 16:45:50 +0800

    添加loading图标

--- src/Button.vue
+++ src/Button.vue
@@ -1,57 +1,70 @@
 <template>
   <button class="g-button">
-    <g-icon 
-      v-if="icon" 
-      :class="`icon-${iconPosition}`"
-      :name="icon"
+    <g-icon
+        v-if="icon"
+        :class="`icon-${iconPosition}`"
+        :name="icon"
     />
+    <g-icon class="icon-loading" name="loading"></g-icon>
     <slot></slot>
-  </button> 
+  </button>
 </template>
 
 <script>
-export default {
-  props: {
-    icon: {},
-    iconPosition: {
-      type: String,
-      default: "left",
-      // 自定义验证函数，return true通过校验，return false不通过校验
-      validator(value) {
-        return value === "left" || value === "right";
+  export default {
+    props: {
+      icon: {},
+      iconPosition: {
+        type: String,
+        default: "left",
+        // 自定义验证函数，return true通过校验，return false不通过校验
+        validator(value) {
+          return value === "left" || value === "right";
+        }
       }
     }
-  }
-};
+  };
 </script>
 
 <style lang="scss" scoped>
-.g-button {
-  display: inline-flex;
-  vertical-align: top;
-  justify-content: center;
-  align-items: center;
-  font-size: var(--font-size);
-  height: var(--button-height);
-  padding: 0 1em;
-  background-color: var(--button-bg);
-  border-radius: var(--border-radius);
-  color: var(--color);
-  border: 1px solid var(--border-color);
-  &:focus {
-    outline: none;
-  }
-  &:hover {
-    border-color: var(--border-color-hover);
-    background-color: var(--button-active-bg);
-  }
-  .icon-right {
-    margin-left: 0.3em;
-    order: 2;
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
   }
 
-  .icon-left {
-    margin-right: 0.3em;
+  .g-button {
+    display: inline-flex;
+    vertical-align: top;
+    justify-content: center;
+    align-items: center;
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    background-color: var(--button-bg);
+    border-radius: var(--border-radius);
+    color: var(--color);
+    border: 1px solid var(--border-color);
+    &:focus {
+      outline: none;
+    }
+    &:hover {
+      border-color: var(--border-color-hover);
+      background-color: var(--button-active-bg);
+    }
+    .icon-right {
+      margin-left: 0.3em;
+      order: 1;
+    }
+
+    .icon-left {
+      margin-right: 0.3em;
+    }
+    .icon-loading {
+      animation: spin 1s infinite linear;
+    }
   }
-}
 </style>
