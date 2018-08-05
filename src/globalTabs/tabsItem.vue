3cc390ac294b337793bd238ad5c97b6992006092
wangkaiwd <1162486176@qq.com>
2018-08-05 22:35:41 +0800

    通过tabs-item的点击事件通知总线中的其它组件实现tab切换

--- src/globalTabs/tabsItem.vue
+++ src/globalTabs/tabsItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-item" :class="{active}" @click="$emit('click',name)">
+  <div class="tabs-item" :class="{active}" @click="onClick">
     <slot></slot>
   </div>
 </template>
@@ -28,7 +28,15 @@
         this.active = name === this.name
       })
     },
-    methods: {}
+    methods: {
+      onClick () {
+        /**
+         * 1. 这里的自定义事件只不过是和tabs中的自定义事件重名了而已
+         * 2. 由于代码逻辑都一样，所以自定义一个同名函数进行逻辑书写
+         */
+        this.eventBus.$emit('updated:selected', this.name)
+      }
+    }
   }
 </script>
 
