d2ad58bdc96d571e2f203e0019a24bef6e9d6877
wangkaiwd <1162486176@qq.com>
2018-08-15 22:44:44 +0800

    在使用v-if的情况下，实现contentWrapper的定位

--- src/popover/popover.vue
+++ src/popover/popover.vue
@@ -1,37 +1,40 @@
 <template>
-    <div class="popover">
-        <!--只有修饰符 @click.stop-->
-        <div class="content-wrapper" ref="contentWrapper" v-show="visible" @click.stop>
-            <!--slot添加事件和class都是没有作用的-->
-            <slot name="content"></slot>
-        </div>
-        <div class="button-wrapper" ref="buttonWrapper" @click.stop="toggleContent">
-            <slot name="trigger"></slot>
-        </div>
+  <div class="popover">
+    <!--只有修饰符 @click.stop-->
+    <div class="content-wrapper" ref="contentWrapper" v-if="visible" @click.stop>
+      <!--slot添加事件和class都是没有作用的-->
+      <slot name="content"></slot>
     </div>
+    <div class="button-wrapper" ref="buttonWrapper" @click.stop="toggleContent">
+      <slot name="trigger"></slot>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'Popover',
     data () {
-      return {visible: false}
+      return {
+        visible: false,
+      }
     },
     mounted () {
-      this.$nextTick(() => {
-        const contentWrapper = this.$refs.contentWrapper
-        const buttonWrapper = this.$refs.buttonWrapper
-        document.body.appendChild(contentWrapper)
-        // 这样获取到的 left,top会被定位的父元素影响
-        // const left = buttonWrapper.offsetLeft + 'px'
-        // const top = buttonWrapper.offsetTop + 'px'
-        // window.scrollY:返回页面的水平滚动距离
-        // document.documentElement.scrollTop: 整张网页的垂直滚动距离
-        // left,top是元素左上角相对于视口左上角的横纵坐标
-        const {left, top} = buttonWrapper.getBoundingClientRect()
-        contentWrapper.style.left = left + 'px'
-        contentWrapper.style.top = top + window.scrollY + 'px'
-      })
+      // 使用v-if页面中不会有这个元素，所以无法获取dom对象
+      // this.$nextTick(() => {
+      // const contentWrapper = this.$refs.contentWrapper
+      // const buttonWrapper = this.$refs.buttonWrapper
+      // document.body.appendChild(contentWrapper)
+      // 这样获取到的 left,top会被定位的父元素影响
+      // const left = buttonWrapper.offsetLeft + 'px'
+      // const top = buttonWrapper.offsetTop + 'px'
+      // window.scrollY:返回页面的水平滚动距离
+      // document.documentElement.scrollTop: 整张网页的垂直滚动距离
+      // left,top是元素左上角相对于视口左上角的横纵坐标
+      // const {left, top} = buttonWrapper.getBoundingClientRect()
+      // contentWrapper.style.left = left + 'px'
+      // contentWrapper.style.top = top + window.scrollY + 'px'
+      // })
     },
     methods: {
       toggleContent () {
@@ -41,39 +44,52 @@
          * 简短代码：看起来精简，但是不方便调试，不太方便阅读
          * 多行代码：看起来占用行数较多，但是方便调试，方便阅读
          */
-        this.visible ?
-          document.addEventListener('click', this.listenClick)
-          :
-          document.removeEventListener('click', this.listenClick)
-        // if (this.visible) {
-        //   console.log('添加事件')
+        // this.visible ?
         //   document.addEventListener('click', this.listenClick)
-        // }
-        // if (!this.visible) {
-        //   console.log('删除事件')
+        //   :
         //   document.removeEventListener('click', this.listenClick)
-        // }
+        if (this.visible) { // 设置为true时，会有一个dom队列
+          // console.log(this.$refs.contentWrapper) // 获取不到dom,队列中的内容会异步执行
+          console.log('添加事件')
+          document.addEventListener('click', this.listenClick)
+          this.moveContent()
+        }
+        if (!this.visible) {
+          console.log('删除事件')
+          document.removeEventListener('click', this.listenClick)
+        }
       },
       listenClick () {
         this.visible = false
         document.removeEventListener('click', this.listenClick)
         console.log('删除事件')
+      },
+      moveContent () {
+        this.$nextTick(() => {
+          const contentWrapper = this.$refs.contentWrapper
+          const buttonWrapper = this.$refs.buttonWrapper
+          document.body.appendChild(contentWrapper)
+          // getBoundingClientRect()方法获取到的left,top是相对视口左上角的坐标
+          const {left, top} = buttonWrapper.getBoundingClientRect()
+          contentWrapper.style.left = left + 'px'
+          contentWrapper.style.top = top + window.scrollY + 'px'
+        })
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-    .popover {
-        display: inline-block;
-        vertical-align: top;
-    }
+  .popover {
+    display: inline-block;
+    vertical-align: top;
+  }
 
-    .content-wrapper {
-        position: absolute;
-        /* 元素设置定位之后的偏移量是根据父元素的宽(left)高(top)来进行计算的 */
-        background-color: skyblue;
-        box-shadow: 0 0 3px rgba(0, 0, 0, .5);
-        transform: translateY(-100%);
-    }
+  .content-wrapper {
+    position: absolute;
+    /* 元素设置定位之后的偏移量是根据父元素的宽(left)高(top)来进行计算的 */
+    background-color: skyblue;
+    box-shadow: 0 0 3px rgba(0, 0, 0, .5);
+    transform: translateY(-100%);
+  }
 </style>
