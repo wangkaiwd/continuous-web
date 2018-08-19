e404ef747efb0dc77740d9c79bf10f0efd9a4e4e
wangkaiwd <1162486176@qq.com>
2018-08-19 22:09:18 +0800

    collapse 功能初步实现

--- src/collapse/collapse.vue
+++ src/collapse/collapse.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="collapse">
-
+    <slot></slot>
   </div>
 </template>
 
@@ -11,5 +11,10 @@
 </script>
 
 <style lang="scss" scoped>
-
+  $border-color: #ddd;
+  $border-radius: 4px;
+  .collapse {
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+  }
 </style>
--- src/collapse/collapseItem.vue
+++ src/collapse/collapseItem.vue
@@ -1,15 +1,59 @@
 <template>
   <div class="collapse-item">
-
+    <div class="title" @click="active = !active">
+      <span>{{title}}</span>
+      <span class="title-icon" :class="{active}"><g-icon name="right-arrow"></g-icon></span>
+    </div>
+    <div class="content" v-if="active">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'CollapseItem'
+    name: 'CollapseItem',
+    props: {
+      title: {
+        type: String,
+        required: true
+      }
+    },
+    data () {
+      return {
+        active: false
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
-
+  $border-color: #ddd;
+  .collapse-item {
+    > .title {
+      padding: 0.5em 1em;
+      min-height: 32px;
+      border-bottom: 1px solid $border-color;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .title-icon {
+        transition: all .4s;
+        display: flex;
+        justify-content: center;
+        align-items: center;}
+      > .active {transform: rotate(90deg);}
+    }
+    &:last-child > .title:last-child {
+      border: 0;
+    }
+    > .content {
+      padding: 0.5em 1em;
+      border-bottom: 1px solid $border-color;
+      transition: all 1s;
+    }
+    &:last-child > .content {
+      border: 0;
+    }
+  }
 </style>
