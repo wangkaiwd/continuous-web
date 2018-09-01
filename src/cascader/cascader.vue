377ecc7c9f12dec83e1bc2bfd47846bbce413cd0
wangkaiwd <1162486176@qq.com>
2018-09-01 16:06:44 +0800

    初步按照想法以及另外一种方式实现级联选择

--- src/cascader/cascader.vue
+++ src/cascader/cascader.vue
@@ -4,17 +4,26 @@
       <!--<slot></slot>-->
 
     </div>
-    <!--<div class="popover" v-if="visible">-->
-    <!--<div class="level1" v-for="(item,index) in options" :key="index">-->
-    <!--{{item.name}}-->
-    <!--<div class="level2">-->
-    <!--<div v-for="(subItem,index) in item.children" :key="index">-->
-    <!--{{subItem.name}}-->
-    <!--</div>-->
-    <!--</div>-->
-    <!--</div>-->
-    <!--</div>-->
-    <cascader-child :optionsItem="options" v-if="visible"></cascader-child>
+    <div class="popover" v-if="visible">
+      <div class="level1">
+        <p @click="activeIndex=index" v-for="(item,index) in options" :key="index">
+          {{item.name}}
+        </p>
+      </div>
+      <div class="level2" v-if="options[activeIndex].children">
+        <p v-for="(subItem,index) in options[activeIndex].children"
+           :key="index"
+           @click="activeIndex2=index">
+          {{subItem.name}}
+        </p>
+      </div>
+      <div class="level3" v-if="options[activeIndex].children && options[activeIndex].children[activeIndex2].children">
+        <p v-for="(subItem2,index) in options[activeIndex].children[activeIndex2].children" :key="index">
+          {{subItem2.name}}
+        </p>
+      </div>
+    </div>
+    <!--<cascader-child :optionsItem="options" v-if="visible"></cascader-child>-->
 
   </div>
 </template>
@@ -34,9 +43,11 @@
     },
     data () {
       return {
-        visible: false
+        visible: false,
+        activeIndex: 0,
+        activeIndex2: 0,
       }
-    }
+    },
   }
 </script>
 
@@ -50,20 +61,27 @@
       border: 1px solid red;
     }
     .popover {
-      /*width: 80px;*/
+      display: inline-flex;
       height: 200px;
       border: 1px solid red;
     }
     .level1 {
       position: relative;
-      /*width: 80px;*/
+      width: 80px;
     }
     .level2 {
-      position: absolute;
-      top: 0;
-      left: 100%;
+      /*position: absolute;*/
+      /*top: 0;*/
+      /*left: 100%;*/
       width: 80px;
       border: 1px solid blue;
+    }
+    .level3 {
+      width: 80px;
+      border: 1px solid pink;
+    }
+    p {
+      cursor: pointer;
     }
   }
 </style>
