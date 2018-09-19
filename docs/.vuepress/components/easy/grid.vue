26ed9af449063b7c3476763c00e42dd81431cf15
wangkaiwd <1162486176@qq.com>
2018-09-19 23:19:00 +0800

    row的gutter属性在使用的时候出现一些问题，需要调整一下

--- docs/.vuepress/components/easy/grid.vue
+++ docs/.vuepress/components/easy/grid.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="grid">
     <g-row class="row">
+      <h4>基础栅格</h4>
       <g-col class="col">1</g-col>
       <g-col class="col" span="14">2</g-col>
       <g-col class="col-right" span="10">3</g-col>
       <g-col class="col" span="12">4</g-col>
       <g-col class="col-right" span="12">5</g-col>
+    </g-row>
+    <g-row class="row" gutter="12">
+      <h4>区块间隔</h4>
+      <g-col class="col">1</g-col>
+      <g-col class="col" span="3">2</g-col>
+      <g-col class="col-right" span="4">3</g-col>
+      <g-col class="col" span="5">4</g-col>
+      <g-col class="col-right" span="6">5</g-col>
+      <g-col class="col" span="6">6</g-col>
+    </g-row>
+    <g-row class="row">
+      <h4>左右偏移</h4>
+      <g-col class="col">1</g-col>
+      <g-col class="col" span="6">2</g-col>
+      <g-col class="col-right" span="4" offset="4">3</g-col>
+      <g-col class="col" span="5" offset="2">4</g-col>
+      <g-col class="col-right" span="6" offset="2">5</g-col>
+      <g-col class="col" span="6" offset="6">6</g-col>
     </g-row>
   </div>
 </template>
@@ -21,6 +40,10 @@
 </script>
 
 <style lang="scss" scoped>
+  * {
+    box-sizing: border-box;
+  }
+
   .col,
   .col-right {
     display: flex;
