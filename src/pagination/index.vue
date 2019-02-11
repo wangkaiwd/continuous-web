429ebe082ed737e51a1112295ed1a27cef01e998
wangkaiwd <1162486176@qq.com>
2019-02-12 00:55:20 +0800

    feat: 分页组件的初步结构搭建

--- src/pagination/index.vue
+++ src/pagination/index.vue
@@ -1,15 +1,60 @@
 <template>
   <div class="pagination">
-
+    <div class="prev">
+      <g-icon name="s-left"></g-icon>
+    </div>
+    <div class="numbers">
+      <div class="first">1</div>
+      <ul>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+      </ul>
+      <div class="last">7</div>
+    </div>
+    <div class="next">
+      <g-icon name="s-right"></g-icon>
+    </div>
   </div>
 </template>
 
 <script>
+  import GIcon from '../icon';
+
   export default {
-    name: 'WdPagination'
+    props: {
+      hideOnSinglePage: {
+        type: Boolean,
+        default: true
+      },
+      total: {
+        type: [String, Number],
+        default: 0
+      },
+      current: {
+        type: [String, Number],
+        default: 1
+      },
+      change: {
+        type: Function,
+        default () {
+
+        }
+      }
+    },
+    name: 'WdPagination',
+    components: { GIcon }
   };
 </script>
 
 <style scoped lang="scss">
-
+  .pagination {
+    display: flex;
+    .numbers {
+      display: flex;
+      ul {display: flex;}
+    }
+  }
 </style>
