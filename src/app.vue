dc0434cd3b96eb60ba6f15d9a92875c737dfe0a0
wangkaiwd <1162486176@qq.com>
2019-02-17 20:44:07 +0800

    feat: 建立table组件，并添加最基础的api

--- src/app.vue
+++ src/app.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="component-wrapper">
+
+  </div>
+  <div class="component-wrapper">
     <wd-pagination :total="600" :current.sync="current"></wd-pagination>
   </div>
 </template>
 <script>
   import WdPagination from './pagination';
+  import WdTable from './table';
 
   export default {
     name: 'App',
-    components: { WdPagination },
+    components: {WdPagination, WdTable},
     data () {
       return {
-        current: 2
+        current: 2,
+        columns: [],
+        dataSource: [
+          {key: 1, name: '小熊猫', score: 83},
+          {key: 2, name: '小袋鼠', score: 90}
+        ]
       };
     },
     mounted () {
@@ -22,6 +31,7 @@
 <style lang="scss" scoped>
   .component-wrapper {
     margin: 40px;
+
     .box {
       width: 100%;
       height: 400px;
---
+++ src/table/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wd-table">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'WdTable',
+    props: {
+      columns: {
+        type: Array,
+        default: []
+      },
+      dataSource: {
+        type: Array,
+        default: []
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  .wd-table {
+
+  }
+</style>
