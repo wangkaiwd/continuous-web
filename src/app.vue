f250e648120751fe2896f30e7706c9c7e88dd3ae
wangkaiwd <1162486176@qq.com>
2019-02-17 23:52:01 +0800

    feat: 一个错误思路，但是确实是这样想的

--- src/app.vue
+++ src/app.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="component-wrapper">
-
-  </div>
-  <div class="component-wrapper">
-    <wd-pagination :total="600" :current.sync="current"></wd-pagination>
+  <div>
+    <div class="component-wrapper">
+      <wd-table
+        :data-source="dataSource"
+        :columns="columns"
+      >
+      </wd-table>
+    </div>
+    <div class="component-wrapper">
+      <wd-pagination :total="600" :current.sync="current"></wd-pagination>
+    </div>
   </div>
 </template>
 <script>
-  import WdPagination from './pagination';
-  import WdTable from './table';
+  import WdPagination from './pagination'
+  import WdTable from './table'
 
   export default {
     name: 'App',
@@ -16,17 +22,29 @@
     data () {
       return {
         current: 2,
-        columns: [],
+        columns: [
+          {
+            key: 1,
+            title: '姓名',
+            dataKey: 'name'
+          },
+          {
+            key: 2,
+            title: '分数',
+            dataKey: 'score'
+          }
+        ],
         dataSource: [
           {key: 1, name: '小熊猫', score: 83},
-          {key: 2, name: '小袋鼠', score: 90}
+          {key: 2, name: '小袋鼠', score: 90},
+          {key: 3, name: '小松鼠', score: 100},
         ]
-      };
+      }
     },
     mounted () {
     },
     methods: {}
-  };
+  }
 </script>
 <style lang="scss" scoped>
   .component-wrapper {
--- src/table/index.vue
+++ src/table/index.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="wd-table">
+    <table>
+      <thead>
+      <tr>
+        <th v-for="col in columns" :key="col.key">
+          {{col.title}}
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="data in dataSource" :key="data.key">
+        <td v-for="item in data">
+          {{item}}
+        </td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -16,7 +32,7 @@
         default: []
       }
     }
-  };
+  }
 </script>
 
 <style lang="scss" scoped>
