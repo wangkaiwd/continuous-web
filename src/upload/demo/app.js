6bbd7120f3a9b827eb0b08b6fbd820edd7f22cd7
wangkaiwd <1162486176@qq.com>
2019-03-14 23:34:12 +0800

    feat: 实现文件的上传和预览

--- src/upload/demo/app.js
+++ src/upload/demo/app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const multer = require('multer');
 const upload = multer({ dest: 'test/' });
@@ -14,6 +15,12 @@ app.get('/', (req, res) => {
 app.post('/profile', upload.single('avatar'), (req, res) => {
   console.log(req.file);
   res.send(req.file.filename);
+});
+app.get('/preview/:img', (req, res) => {
+  fs.readFile(`./test/${req.params.img}`, (err, data) => {
+    if (err) res.send('read file err');
+    res.send(data);
+  });
 });
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`);
