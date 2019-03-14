59a98f876d46597caa5d7c37b7a2694932bf18dd
wangkaiwd <1162486176@qq.com>
2019-03-14 23:20:39 +0800

    feat: 图片上传成功，并将路径返回

--- src/upload/demo/app.js
+++ src/upload/demo/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: 'test/' });
 const app = express();
 const port = 9000;
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.get('/', (req, res) => {
   res.send('hello express!');
 });
 app.post('/profile', upload.single('avatar'), (req, res) => {
-  // console.log(req.file.filename);
+  console.log(req.file);
   res.send(req.file.filename);
 });
 app.listen(port, () => {
