b52a2ed4b2247bfcce8099212fb93a0d708df2fb
wangkaiwd <1162486176@qq.com>
2019-03-14 23:58:04 +0800

    feat: res.sendFile的简单理解

--- src/upload/demo/app.js
+++ src/upload/demo/app.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const express = require('express');
 const multer = require('multer');
 const upload = multer({ dest: 'test/' });
@@ -13,13 +12,14 @@ app.get('/', (req, res) => {
   res.send('hello express!');
 });
 app.post('/profile', upload.single('avatar'), (req, res) => {
-  console.log(req.file);
+  // console.log(req.file);
   res.send(req.file.filename);
 });
 app.get('/preview/:img', (req, res) => {
-  fs.readFile(`./test/${req.params.img}`, (err, data) => {
-    if (err) res.send('read file err');
-    res.send(data);
+  // 相当于原生nodejs的fs.readFile
+  // 通过设置绝对路径，来将对应的文件信息返回，并通过回调函数处理错误
+  res.sendFile(`./test/${req.params.img}`, { root: __dirname }, err => {
+    if (err) res.status(404).send('Not Found');
   });
 });
 app.listen(port, () => {
