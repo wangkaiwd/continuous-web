0569a8cca2c991f024d4136fbd6e7bcdd44b73a2
wangkaiwd <1162486176@qq.com>
2019-03-25 23:49:21 +0800

    feat: 建立sticky文件夹

---
+++ src/sticky/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wd-sticky">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'WdSticky'
+  };
+</script>
+
+<style scoped lang="scss">
+  .wd-sticky {
+
+  }
+</style>
