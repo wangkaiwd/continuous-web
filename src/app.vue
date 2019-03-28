70e219048274731fc9712899878565819bcc1cff
wangkaiwd <1162486176@qq.com>
2019-03-28 23:45:02 +0800

    feat: 初步书写sticky组件，感觉比较复杂

--- src/app.vue
+++ src/app.vue
@@ -18,85 +18,50 @@
           prop 和 组件名：会有大小写转换的过程，所以在html模板中书写的时候最好使用kebab-case短横线格式
           事件名：不会进行任何自动化的转换，但是推荐使用kebab-case短横线命名格式
       -->
-      <div style="margin-bottom: 20px">
-        文件内容不能超过500M
+      <div class="box">
+        <g-sticky>
+          <div class="header">
+            定位
+          </div>
+        </g-sticky>
       </div>
-      <wd-uploader
-        name="file"
-        method="post"
-        action="http://localhost:9000/profile"
-        accept="image/*"
-        :file-list.sync="fileList"
-      >
-        <g-button icon="upload">点击上传</g-button>
-      </wd-uploader>
     </div>
   </div>
 </template>
 <script>
-  import WdUploader from './upload';
-  import GButton from './button';
+  import GSticky from './sticky';
 
   export default {
     name: 'App',
-    components: { WdUploader, GButton },
+    components: { GSticky },
     data () {
-      return {
-        current: 2,
-        selectItem: [],
-        columns: [ // 表头，对应列
-          { id: 1, dataKey: 'name', title: '姓名' },
-          { id: 2, dataKey: 'position', title: '位置' },
-          { id: 3, dataKey: 'skill', title: '技能' },
-          { id: 4, dataKey: 'score', title: '分数' },
-          { id: 5, dataKey: 'age', title: '年龄' },
-        ],
-        // 排序规则： 升序：asc, 降序：desc, 默认序：false, 不能点击排序：不传入对应字段
-        orderBy: {
-          score: 'desc',
-          age: 'asc',
-          skill: false
-        },
-        dataSource: [ // 数据源，对应行
-          { id: 1, name: '王昭君', position: '法师', skill: 4, score: 70, age: 20, description: '这是一个很漂亮的英雄，擅长使用冰雪法术' },
-          { id: 2, name: '狄仁杰', position: '射手', skill: 4, score: 80, age: 12, description: '这是一个很帅气的英雄,掌管六扇门' },
-          { id: 3, name: '李元芳', position: '打野', skill: 1, score: 40, age: 14 },
-          { id: 4, name: '姜子牙', position: '辅助', skill: 2, score: 30, age: 82 },
-          { id: 5, name: '程咬金', position: '上单', skill: 6, score: 90, age: 62 },
-        ],
-        loading: false,
-        fileList: []
-      };
+      return {};
     },
     mounted () {
     },
-    methods: {
-      // 模拟排序
-      onSort () {
-        this.loading = true;
-        setTimeout(() => {
-          // 通过ajax请求最新的排序数据
-          // 模拟score字段排序
-          this.dataSource.sort((a, b) => a.score - b.score);
-          this.loading = false;
-        }, 3000);
-      }
-    }
+    methods: {}
   };
 </script>
 <style lang="scss" scoped>
   .component-wrapper {
     margin: 40px;
-
+    max-height: 100vh;
+    display: flex;
+    /*align-items: center;*/
+    justify-content: center;
+    overflow: auto;
     .box {
-      width: 100%;
-      height: 400px;
-      background-color: #ddd;
+      height: 2000px;
+      width: 200px;
+      background-color: green;
+    }
+    .header {
       display: flex;
       align-items: center;
       justify-content: center;
-      font-size: 100px;
-      color: red;
+      height: 100px;
+      width: 100%;
+      background-color: red;
     }
     .space {
       margin: 10px;
--- src/sticky/index.vue
+++ src/sticky/index.vue
@@ -1,17 +1,34 @@
 <template>
   <div class="wd-sticky">
-
+    <div class="wd-sticky-placeholder" ref="stickPlaceholder">
+      <div class="wd-sticky-wrapper" ref="stickyWrapper">
+        <slot></slot>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'WdSticky'
+    name: 'WdSticky',
+    mounted () {
+      const { height, top, left } = this.$refs.stickyWrapper;
+      const placeholder = this.$refs.stickPlaceholder;
+      placeholder.style.height = height + 'px';
+    }
   };
 </script>
 
 <style scoped lang="scss">
   .wd-sticky {
-
+    &-placeholder {
+      width: 100%;
+    }
+    &-wrapper {
+      position: fixed;
+      width: 100%;
+      left: 0;
+      top: 0;
+    }
   }
 </style>
