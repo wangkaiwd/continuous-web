4ded388244e8775cfd660ee0358a4145d9eed1ba
wangkaiwd <1162486176@qq.com>
2019-09-28 19:21:20 +0800

    feat: 删除app中多余的文件

--- src/app.vue
+++ src/app.vue
@@ -1,170 +1,21 @@
 <template>
   <div>
     <div class="component-wrapper">
-      <!--<self-cascader
-        :options.sync="options"
-        :selected.sync="selected"
-        :load-data="loadData"
-      >
-      </self-cascader>-->
-      <!--<self-menu :selected.sync="selected">
-        <sub-menu name="1">
-          <template #title>
-            子菜单
-          </template>
-          <menu-item name="1-1">
-            子菜单1
-          </menu-item>
-          <menu-item name="1-2">
-            子菜单2
-          </menu-item>
-          <sub-menu name="about">
-            <template #title>
-              关于
-            </template>
-            <menu-item name="about-1">
-              关于-1
-            </menu-item>
-          </sub-menu>
-        </sub-menu>
-        <menu-item name="2">
-          菜单1
-        </menu-item>
-        <menu-item name="3">
-          菜单2
-        </menu-item>
-      </self-menu>-->
-      <self-menu :selected.sync="selected" vertical>
-        <sub-menu name="1">
-          <template #title>
-            子菜单
-          </template>
-          <menu-item name="1-1">
-            子菜单1
-          </menu-item>
-          <menu-item name="1-2">
-            子菜单2
-          </menu-item>
-          <sub-menu name="about">
-            <template #title>
-              关于
-            </template>
-            <menu-item name="about-1">
-              关于-1
-            </menu-item>
-          </sub-menu>
-        </sub-menu>
-        <menu-item name="2">
-          菜单1
-        </menu-item>
-        <menu-item name="3">
-          菜单2
-        </menu-item>
-      </self-menu>
     </div>
   </div>
 </template>
 <script>
-  import SelfCascader from './cascader/cascader';
-  import SelfButton from './button';
-  import chinaCities from './cascader/db';
-  import SelfMenu from './menu/menu';
-  import SubMenu from './menu/subMenu';
-  import MenuItem from './menu/menuItem';
 
-  /*
-  * 目前注意点：
-  *   1. 如何递归处理数据，实现数据动态一层一层的加载
-  *   2. 当涉及到动态数据的时候，要为数据添加一个新的属性:isLeaf
-  * */
 
-  const prettyCities = (chinaCitiesCopy) => {
-    return chinaCitiesCopy.map(city => {
-      if (city.child) {
-        city.child = prettyCities(city.child);
-      }
-      const tempObject = {
-        value: city.zipcode || city.id,
-        label: city.name,
-      };
-      city.child && (tempObject.children = city.child);
-      return tempObject;
-    });
-  };
-  const ajax = (id) => {
-    const chinaCitiesCopy = JSON.parse(JSON.stringify(chinaCities));
-    const cities = prettyCities(chinaCitiesCopy);
-    const citiesCopy = JSON.parse(JSON.stringify(cities));
-    let result = null;
-    if (typeof id === 'undefined') {
-      result = citiesCopy.map(item => {
-        const tempObj = { value: item.value, label: item.label, isLeaf: true };
-        item.children && (tempObj.isLeaf = false);
-        return tempObj;
-      });
-    } else {
-      const iterate = (array, id) => {
-        let result = {};
-        for (let i = 0; i < array.length; i++) {
-          const item = array[i];
-          if (item.value === id) {
-            result = item;
-            result.isLeaf = true;
-            if (result.children) {
-              result.isLeaf = false;
-              result.children.map(item => {
-                item.isLeaf = true;
-                if (item.children) {
-                  item.isLeaf = false;
-                  delete item.children;
-                }
-              });
-            }
-            break;
-          } else {
-            if (item.children) {
-              result = iterate(item.children, id);
-              if (Object.keys(result).length > 0) {
-                break;
-              }
-            }
-          }
-        }
-        return result;
-      };
-      result = iterate(citiesCopy, id);
-    }
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(result);
-      }, 2000);
-    });
-  };
   export default {
     name: 'App',
-    components: { SelfCascader, SelfButton, SelfMenu, SubMenu, MenuItem },
+    components: {},
     data () {
-      return {
-        options: [],
-        selected: ''
-      };
+      return {};
     },
     mounted () {
-      this.getFirstLevel();
     },
-    methods: {
-      // 帮用户处理通过id更新options的操作
-      loadData (updateOptions) {
-        const { selected } = this;
-        const finalValue = selected[selected.length - 1].value;
-        ajax(finalValue).then(
-          res => {updateOptions(res);}
-        );
-      },
-      getFirstLevel () {
-        ajax().then(res => this.options = res);
-      }
-    }
+    methods: {}
   };
 </script>
 <style lang="scss" scoped>
