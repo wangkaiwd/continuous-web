ff0963b5a7f0357b9721a7007bc76e6abb17df7f
wangkaiwd <1162486176@qq.com>
2021-03-27 22:23:54 +0800

    feat: get package current version

--- 00.demo/lib/core/index.js
+++ 00.demo/lib/core/index.js
@@ -1,5 +1,10 @@
-function core () {
+// require支持的文件类型：
+// .js: 读取该文件，并通过node.js中vm模块来执行读取到的字符串
+// .json: 通过fs.readFile读取到文件内容，执行JSON.parse即可
+const pkg = require('../../package.json');
 
+function core (argv) {
+  console.log(pkg.version);
 }
 
 module.exports = core;
--- 00.demo/readme.md
+++ 00.demo/readme.md
@@ -4,3 +4,7 @@
 * 命令模块：command
 * 模型模块：model
 * 工具模块：util
+
+### knowledge
+
+* require支持的文件后缀: `.js`,`.json`,`.node`，其它后缀的文件都会按照`js`文件进行解析
