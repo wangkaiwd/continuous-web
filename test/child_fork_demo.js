e0c45fd8df54fc04c9170940f42dea29ea7adbec
wangkaiwd <1162486176@qq.com>
2021-04-04 00:32:25 +0800

    feat: more events of child process

--- test/child_fork_demo.js
+++ test/child_fork_demo.js
@@ -2,5 +2,6 @@ console.log('fork node.js process');
 process.on('message', (msg) => {
   console.log('child got message', msg);
 });
+process.send('I am child process!');
 
 console.log('child pid:', process.pid);
--- test/child_process.js
+++ test/child_process.js
@@ -27,29 +27,40 @@ const { exec, execFile, spawn, fork } = require('child_process');
 // });
 
 // spawn 产生的子进程，需要通过stdout(可读流)来获取到命令输出内容
-// const subprocess = spawn('ls', ['-l']);
-// // console.log(subprocess.pid, process.pid);
-//
-// const bufs = [];
-// subprocess.stdout.on('data', (chunk) => {
-//   bufs.push(chunk);
-// });
-// subprocess.stderr.on('data', (err) => {
-//   console.log('error---------', err);
-// });
-// subprocess.stdout.on('end', () => {
-//   console.log(Buffer.concat(bufs).toString());
-// });
+// options中可以传入: {stdio: 'inherit'}来实现npm install，这样会将父进程的安装过程全部显示，包括loading等效果
+const subprocess = spawn('ls', ['-l']);
+// console.log(subprocess.pid, process.pid);
+
+subprocess.stdout.on('data', (chunk) => {
+  console.log(chunk.toString());
+});
+subprocess.stderr.on('data', (chunk) => {
+  console.log(chunk.toString());
+});
+subprocess.on('error', (e) => {
+  console.log('error', e);
+  // 错误时用code1来退出进程
+  process.exit(1);
+});
+
+subprocess.on('exit', (e) => {
+  console.log('execute successful:' + e);
+  process.exit(e);
+});
+
 // 比较：
 // 由于spawn用到了stream来一点一点读取输出的内容，所以适合开销比较大的耗时任务(流的优点)(如:npm install)
 // exec/execFile适合开销比价小的任务
 
-// 创建一个新的Node进程
-const child = fork(path.resolve(__dirname, 'child_fork_demo.js'));
-
-// callback is invoked after the message is sent but before the child may have received it.
-child.send('hello child process', () => {
-  child.disconnect();
-});
-
-console.log('main pid:', process.pid);
+// 创建一个新的Node进程，来执行耗时任务，可以进行父子进程之间参数传递
+// const child = fork(path.resolve(__dirname, 'child_fork_demo.js'));
+//
+// // callback is invoked after the message is sent but before the child may have received it.
+// child.send('hello child process', () => {
+//   // child.disconnect();
+// });
+// child.on('message', (msg) => {
+//   console.log('parent got message', msg);
+// });
+//
+// console.log('main pid:', process.pid);
