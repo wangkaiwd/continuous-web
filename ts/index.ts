b8d04ce49d53901595f221a76a30f11433172e1a
wangkaiwd <1162486176@qq.com>
2021-05-05 21:16:39 +0800

    feat: supply some demo

--- ts/index.ts
+++ ts/index.ts
@@ -1,8 +1,12 @@
-const echo = (arg) => {
-  return arg;
-};
+// const echo = <T> (arg: T): T => {
+//   return arg;
+// };
+//
+// // 函数无法进行类型推导
+// const result = echo(1);
 
-// 函数无法进行类型推导
-const result = echo('str');
-
-console.log(result);
+// const swap = <T, U> (arr: [T, U]): [U, T] => {
+//   return [arr[1], arr[0]];
+// };
+//
+// const result = swap(['a', 1]);
