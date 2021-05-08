862e611569292700b6b5530adc1387e2bfc309b1
wangkaiwd <1162486176@qq.com>
2021-05-09 00:56:33 +0800

    feat: try watch effect api

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -2,11 +2,12 @@
   <div class="app">
     <h3>vue3 vite ts</h3>
     <h4>{{ todos.result?.name }}</h4>
+    <h3>{{ count }}</h3>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue';
+import { defineComponent, reactive, ref, watchEffect } from 'vue';
 
 interface IResult {
   id: number;
@@ -23,8 +24,18 @@ export default defineComponent({
   components: {},
   setup () {
     const todos = reactive<TodoProps>({ result: null });
+    const count = ref<number>(0);
+    count.value.toFixed();
+    watchEffect(() => {
+      // why log count not trigger page update
+      console.log('count', count.value);
+    });
+    setInterval(() => {
+      count.value++;
+    }, 1000);
     return {
-      todos
+      todos,
+      count
     };
   }
 });
