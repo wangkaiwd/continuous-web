7c7435b8e334579ebd3d9c50155be1e88b91955a
wangkaiwd <1162486176@qq.com>
2021-05-09 18:19:38 +0800

    feat: set type for component props

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -3,12 +3,14 @@
     <h3>vue3 vite ts</h3>
     <h4>{{ todos.result?.name }}</h4>
     <h3>{{ count }}</h3>
+    <hello-world :info="info"></hello-world>
   </div>
 </template>
 
 <script lang="ts">
 
-import { defineComponent, reactive, watchEffect } from 'vue';
+import { defineComponent, reactive, ref, watchEffect } from 'vue';
+import HelloWorld, { IInfo } from './components/hello-world.vue';
 
 interface IResult {
   id: number;
@@ -22,20 +24,23 @@ interface TodoProps {
 
 export default defineComponent({
   name: 'App',
+  components: { HelloWorld },
   setup () {
     const todos = reactive<TodoProps>({ result: null });
+    const info = reactive<IInfo>({ age: 10, name: 'AXXf1u' });
     const count = ref<number>(0);
     count.value.toFixed();
-    watchEffect(() => {
-      // why log count not trigger page update
-      console.log('count', count.value);
-    });
+    // watchEffect(() => {
+    //   // why log count not trigger page update
+    //   console.log('count', count.value);
+    // });
     setInterval(() => {
       count.value++;
     }, 1000);
     return {
       todos,
-      count
+      count,
+      info
     };
   }
 });
---
+++ vite-project/src/components/hello-world.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello-world">
+    <h2>{{ info.name }}</h2>
+    <h3>{{ info.age }}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, reactive, toRefs } from 'vue';
+
+export interface IInfo {
+  name: string;
+  age: number
+}
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    info: {
+      type: Object as PropType<IInfo>,
+      required: true
+    }
+  },
+  setup () {
+    const state = reactive({});
+    return {
+      ...toRefs(state)
+    };
+  },
+});
+</script>
+
+<style lang="less" scoped>
+.hello-world {
+
+}
+</style>
