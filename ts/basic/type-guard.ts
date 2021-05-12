96c297677b18db6757ff99950d8f78ebd9683f08
wangkaiwd <1162486176@qq.com>
2021-05-13 00:01:42 +0800

    feat: add some annotation of type guard

--- ts/basic/type-guard.ts
+++ ts/basic/type-guard.ts
@@ -6,7 +6,6 @@
 // 4. Object.prototype.toString.call (all-round)
 
 // 也可以用in来进行不同object类型区分
-
 function fn (val: string | number) {
   if (typeof val === 'string') {
     val.replace('', '');
@@ -14,5 +13,11 @@ function fn (val: string | number) {
     val.toFixed(2);
   }
 }
+
+// discriminate union
+
+// type predicate
+
+// null: 内部函数类型检测无法智能判断
 
 export {};
