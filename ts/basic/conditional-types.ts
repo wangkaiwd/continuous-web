321064a30c1bb56fdda44e61ede6db8b9f6473d4
wangkaiwd <1162486176@qq.com>
2021-05-15 20:53:12 +0800

    feat: complete conditional type of typescript

--- ts/basic/conditional-types.ts
+++ ts/basic/conditional-types.ts
@@ -36,5 +36,34 @@ type NonNullable<T> = T extends null | undefined ? never : T
 type MyNonNullable = NonNullable<string | boolean | null | undefined>
 
 //infer: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html#type-inference-in-conditional-types
+// https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#inferring-within-conditional-types
+function getSchool (name: string, age: number) {
+  return {
+    name,
+    age
+  };
+}
+
+type ReturnType<T extends (...args: any[]) => any> = T extends (...args: any[]) => infer R ? R : never
+
+type MyReturnType = ReturnType<typeof getSchool>
+
+type Parameters<T extends (...args: any[]) => any> = T extends (...args: infer R) => any ? R : never
+
+type MyParameters = Parameters<typeof getSchool>
+
+class Person {
+  name: string;
+
+  constructor (name: string) {
+    this.name = name;
+  }
+}
+
+type ConstructorParameters<T extends new(...args: any[]) => any> = T extends new(...args: infer P) => any ? P : never
+type MyConstructorParameters = ConstructorParameters<typeof Person>
+
+type InstanceType<T extends new(...args: any[]) => any> = T extends new(...args: any[]) => infer R ? R : never
+type MyInstanceType = InstanceType<typeof Person>
 
 export {};
