d6a51c241620411feff738396d978dae8c2296da
wangkaiwd <1162486176@qq.com>
2021-05-17 00:23:12 +0800

    feat: declare merge of typescript

--- ts/basic/modules.ts
+++ ts/basic/modules.ts
@@ -1,1 +1,7 @@
- // commonjs 和 esModule 混合使用
+// commonjs 和 esModule 混合使用
+// require 引入的内容没有类型提示
+
+// import + require引入才会有类型提示
+// import fs = require('fs');
+
+// import pic from './a.png';
--- ts/basic/test.d.ts
+++ ts/basic/test.d.ts
@@ -1,1 +1,18 @@
-declare const jQuery: (selector: string) => any;
+// 通过declare来声明文件，这个声明只是为了防止报错，没有任何意义
+declare function jQuery (selector: string): {
+  css (val: object): void
+};
+
+// 支持import 'xxx.png'
+declare module '*.png' {
+  const png: any;
+  export default png;
+}
+
+// declare merge: function with namespace
+// https://www.typescriptlang.org/docs/handbook/declaration-merging.html#merging-namespaces-with-classes-functions-and-enums
+declare namespace jQuery {
+  namespace fn {
+    function extend (): void
+  }
+}
